441a07ff609c0f3d0f82681b25f88696d386e819
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2021-05-04 12:12:53 -0700

    updated for resubmission

--- Assignments/Events-Homework/events_app/models.py
+++ Assignments/Events-Homework/events_app/models.py
@@ -3,31 +3,33 @@ from events_app import db
 from sqlalchemy.orm import backref
 
 class Guest(db.Model):
-    # - id: primary key
-    id = db.Column(db.Integer, primary_key=True)
-    # - name: String column
-    name = db.Column(db.String)
-    # - email: String column
-    email = db.Column(db.String)
-    # - phone: String column
-    phone = db.Column(db.String)
-    # - events_attending: relationship to "Event" table with a secondary table
-    events_attending = db.relationship('Event', secondary='event_guest', back_populates='guest')
+  # - id: primary key
+  id = db.Column(db.Integer, primary_key=True)
+  # - name: String column
+  name = db.Column(db.String)
+  # - email: String column
+  email = db.Column(db.String)
+  # - phone: String column
+  phone = db.Column(db.String)
+  # - events_attending: relationship to "Event" table with a secondary table
+  events_attending = db.relationship('Event', secondary='guest_event', back_populates='guests')
 
 
 
 class Event(db.Model):
+  """Events Model"""
   # - id: primary key
-    id = db.Column(db.Integer, primary_key=True)
-    # - title: String column
-    title = db.Column(db.String(80), nullable=False)
-    # - description: String column
-    description = db.Column(db.String(50), nullable=False)
-    # - date_and_time: DateTime column
-    date_and_time = db.Column(db.DateTime, nullable=False)
-    # - guests: relationship to "Guest" table with a secondary table
-    num_guests = db.Column(db.Integer, nullable = False)
-    guests = db.relationship('Guest', secondary='guest_event', back_populates='events_attending')
+  id = db.Column(db.Integer, primary_key=True)
+  # - title: String column
+  title = db.Column(db.String(80), nullable=False)
+  # - description: String column
+  description = db.Column(db.String(50), nullable=False)
+  # - date_and_time: DateTime column
+  date_and_time = db.Column(db.DateTime, nullable=False)
+  # - guests: relationship to "Guest" table with a secondary table
+  num_guests = db.Column(db.Integer, nullable = False)
+  guests = db.relationship('Guest', secondary='guest_event', back_populates='events_attending')
+
 
 
 
